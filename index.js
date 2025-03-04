0d913abc84c80166c5a7c6a1f05772488c1e57fc
vera.bolgarskay <verabolgarskay@gmail.com>
2025-03-04 16:11:10 +0500

    task 2

--- index.js
+++ index.js
@@ -7,19 +7,17 @@ const comments = getComments();
 console.log('Please, write your command!');
 readLine(processCommand);
 
-
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
     return filePaths.map(path => readFile(path));
 }
 
 function getComments() {
-    const regexp = /\/\/ TODO .*\r/g; 
+    const regexp = /\/\/ TODO .*\r/g;
     const answer = [];
     for (const content of files) {
-        answer.push([...content.matchAll(regexp)].map(match => match[0])); 
+        answer.push([...content.matchAll(regexp)].map(match => match[0]));
     }
-
     return answer;
 }
 
@@ -28,10 +26,11 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            comments.flat(1).forEach(comment => console.log(comment));
+            break;
         default:
             console.log('wrong command');
             break;
     }
 }
-
-// TODO you can do it!
