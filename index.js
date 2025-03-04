f44ac46276726cadcfadbfe7e5e22688bf357852
kitekatya <prostopostya221@yandex.ru>
2025-03-04 16:03:15 +0500

    add function

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const comments = getComments();
 
 console.log('Please, write your command!');
 readLine(processCommand);
 
+
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
     return filePaths.map(path => readFile(path));
+}
+
+function getComments() {
+    const regexp = /\/\/ TODO .*\r/g; 
+    const answer = [];
+    for (const content of files) {
+        answer.push([...content.matchAll(regexp)].map(match => match[0])); 
+    }
+
+    return answer;
 }
 
 function processCommand(command) {
